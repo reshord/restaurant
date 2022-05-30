c01ce0449fee6eca2ca75b236bc390fa76ab536c
Dmitriy <littlebig11234@gmail.com>
2022-05-30 15:55:19 +0300

    change-adaptive2

--- index.js
+++ index.js
@@ -43,7 +43,118 @@ const render = arr => {
           </div>
               ` 
               content.append(div)
+              
+              
     })
 }
 
+
+
+
 render(arr)
+
+
+window.addEventListener('click', (event) => {
+    const btn = event.target.classList == 'card-btn'
+    const modalBlock = document.querySelector('.modal-global_block')
+    if(btn) {
+        modalBlock.innerHTML = ''
+        const card = event.target.closest('.card')
+        const productInfo = {
+            title: card.querySelector('.card-title').innerHTML,
+            img: card.querySelector('.card-image img').getAttribute('src'),
+            weight: card.querySelector('.card-weight').innerHTML,
+            price: card.querySelector('.card-price').innerHTML,
+        }
+        const moduleCart = document.createElement('div')
+              moduleCart.innerHTML =  `
+              <div class="modal-block-image">
+              <img style="margin-top: 20px; width: 170px; height: 110px;" class="modal-image" src="${productInfo.img}" alt="">
+          </div>
+          <div class="modal-block">
+              <div class="card-title">${productInfo.title}c</div>
+              <div class="card-weight">${productInfo.weight}</div>
+              <div class="card-price">${productInfo.price}</div>
+              <button class="card-btn btn">Order</button>
+          </div>
+        `
+        modalBlock.appendChild(moduleCart)
+    }
+})
+
+
+// btn.forEach(btnEl => {
+//     btnEl.addEventListener('click', (event) => {
+//         if(btnEl) {
+//             const card = event.target.closest('.card')
+//             const infoProduct = {
+//                 title: card.querySelector('.cart-title').innerHTML
+//             }
+//             console.log(infoProduct.title);
+//         }
+//     })
+// })
+
+
+const modalOpen = document.querySelector('.favourites'),
+      modal = document.querySelector('.modal'),
+      modalClose = document.querySelector('.modal-close')
+
+modalOpen.addEventListener('click', () => {
+    modal.classList.add('active')
+})
+
+modalClose.addEventListener('click', () => {
+    modal.classList.remove('active')
+})
+
+
+
+
+// const wrapperBlock = document.querySelectorAll('.wrapper-block__sidebar__block1')
+const wrapperBlock = document.querySelector('.wrapper-block__sidebar_element')
+
+
+
+
+const sortProduct = (arr) => {
+    arr.forEach(product => {
+        const div = document.createElement('div')
+        div.innerHTML = product.title
+        div.style.color = 'white'
+        div.style.marginLeft = '25px'
+        div.style.marginTop = '12px'
+        div.style.cursor = 'pointer'
+
+        wrapperBlock.append(div)
+    })
+
+    // wrapperBlock.forEach(label => {
+    //     const itemEl = label.children[1]
+    //     itemEl.addEventListener('click', () => {
+    //         content.innerHTML = ''
+    //         const res = arr.filter(el => {
+    //             if(el.title === itemEl.innerHTML) {
+    //                 const div = document.createElement('div')
+    //                       div.classList = 'card-block'  
+    //                       div.innerHTML = `
+    //                             <div class="card">
+    //                             <div class="card-title">${el.title}</div>
+    //                             <div class="card-image">
+    //                                 <img src="${el.img}" alt="">
+    //                             </div>
+    //                             <div class="card-footer">
+    //                                 <div class="card-weight">${el.weight}</div>
+    //                                 <div class="card-price">${el.price}</div>
+    //                             </div>
+    //                             <button class="card-btn">Delivery</button>
+    //                         </div>
+    //                             ` 
+    //           content.append(div)
+    //             }
+    //         })
+    //     })
+    // })
+}
+
+sortProduct(arr)
