ea74d148297ef85825857b7642119221a410883f
Dmitriy <littlebig11234@gmail.com>
2022-05-31 14:12:15 +0300

    change-sort

--- index.js
+++ index.js
@@ -3,25 +3,31 @@ const content = document.querySelector('.wrapper-block__content')
 
 const arr = [
     {
-        title: 'Big Mac', img: './big-mac.jpg', weight: '220gr', price: '600p'
+        title: 'Big Mac', class: 'Burgers and rolls', img: './big-mac.jpg', weight: 220, price: 600
     },
     {
-        title: 'Hamburger', img: './hamburger.jpg', weight: '140gr', price: '200p'
+        title: 'Hamburger', class: 'Happy Mil', img: './hamburger.jpg', weight: 140, price: 200
     },
     {
-        title: 'Double Cheeseburger', img: './double-cheeseburger.jpg', weight: '180gr', price: '260p'
+        title: 'Double Cheeseburger', class: 'Burgers and rolls', img: './double-cheeseburger.jpg', weight: 180, price: 260
     },
     {
-        title: 'Run Tasty', img: './run-tasty.jpg', weight: '260gr', price: '300p'
+        title: 'Run Tasty', img: './run-tasty.jpg', weight: 260, price: 300
     },
     {
-        title: 'McCicken', img: './McCicken.jpg', weight: '200gr', price: '250p'
+        title: 'McCicken', class: 'Burgers and rolls', img: './McCicken.jpg', weight: 200, price: 250
     },
     {
-        title: 'Fish Roll', img: './fish-roll.jpg', weight: '320gr', price: '340p'
+        title: 'Fish Roll', img: './fish-roll.jpg', weight: 320, price: 340
     },
     {
-        title: 'Roll with shrimp', img: './roll-with-shrimp.jpg', weight: '280gr', price: '310p'
+        title: 'Roll with shrimp', img: './roll-with-shrimp.jpg', weight: 280, price: 310
+    },
+    {
+        title: 'Chicken McNuggets', class: 'Happy Mil', img: './ChickenMcNuggets.jpg', weight: 190, price: 210
+    },
+    {
+        title: 'McMuffin', class: 'Breakfast', img: './McMuffin.jpg', weight: 230, price: 290
     },
 ]
 
@@ -36,8 +42,8 @@ const render = arr => {
                   <img src="${card.img}" alt="">
               </div>
               <div class="card-footer">
-                  <div class="card-weight">${card.weight}</div>
-                  <div class="card-price">${card.price}</div>
+                  <div class="card-weight">${card.weight}gr</div>
+                  <div class="card-price">${card.price}p</div>
               </div>
               <button class="card-btn">Delivery</button>
           </div>
@@ -53,13 +59,17 @@ const render = arr => {
 
 render(arr)
 
+let counterNum = 0;
 
 window.addEventListener('click', (event) => {
     const btn = event.target.classList == 'card-btn'
-    const modalBlock = document.querySelector('.modal-global_block')
+    const modalBlock = document.querySelector('.modal-global_block'),
+          counter = document.querySelector('.counter-radius')
+          
     if(btn) {
         modalBlock.innerHTML = ''
         const card = event.target.closest('.card')
+        counter.innerHTML = ++counterNum
         const productInfo = {
             title: card.querySelector('.card-title').innerHTML,
             img: card.querySelector('.card-image img').getAttribute('src'),
@@ -80,7 +90,13 @@ window.addEventListener('click', (event) => {
         `
         modalBlock.appendChild(moduleCart)
     }
+    
+    
 })
+
+
+
+
 
 
 // btn.forEach(btnEl => {
@@ -108,26 +124,75 @@ modalClose.addEventListener('click', () => {
     modal.classList.remove('active')
 })
 
+const check = document.querySelector('#check')
+const check1 = document.querySelector('#check1')
+
+const sortProduct = (arr) => {
+    check.addEventListener('click', () => {
+        check1.checked = false
+        
+        content.innerHTML = ''
+        
+        if(check.checked === true) {
+
+            arr.sort((a,b) => a.price - b.price).forEach(el => {
+                const div = document.createElement('div')
+                      div.innerHTML = `
+                      <div class="card">
+                      <div class="card-title">${el.title}</div>
+                      <div class="card-image">
+                          <img src="${el.img}" alt="">
+                      </div>
+                      <div class="card-footer">
+                          <div class="card-weight">${el.weight}gr</div>
+                          <div class="card-price">${el.price}p</div>
+                      </div>
+                      <button class="card-btn">Delivery</button>
+                  </div>
+                      `
+
+                      content.append(div)
+            }) 
+        } 
+    })
+    
+    
+}
+sortProduct(arr)
+
+const sortProduct2 = (arr) => {
+    check1.addEventListener('click', () => {
+        check.checked = false
+        content.innerHTML = ''
+        if(check1.checked === true) {
+
+            arr.sort((a,b) => a.weight - b.weight).forEach(el => {
+                const div = document.createElement('div')
+                      div.innerHTML = `
+                      <div class="card">
+                      <div class="card-title">${el.title}</div>
+                      <div class="card-image">
+                          <img src="${el.img}" alt="">
+                      </div>
+                      <div class="card-footer">
+                          <div class="card-weight">${el.weight}gr</div>
+                          <div class="card-price">${el.price}p</div>
+                      </div>
+                      <button class="card-btn">Delivery</button>
+                  </div>
+                      `
+
+                      content.append(div)
+            }) 
+        } 
+     })
+}
+sortProduct2(arr)
 
 
 
 // const wrapperBlock = document.querySelectorAll('.wrapper-block__sidebar__block1')
-const wrapperBlock = document.querySelector('.wrapper-block__sidebar_element')
 
-
-
-
-const sortProduct = (arr) => {
-    arr.forEach(product => {
-        const div = document.createElement('div')
-        div.innerHTML = product.title
-        div.style.color = 'white'
-        div.style.marginLeft = '25px'
-        div.style.marginTop = '12px'
-        div.style.cursor = 'pointer'
-
-        wrapperBlock.append(div)
-    })
 
     // wrapperBlock.forEach(label => {
     //     const itemEl = label.children[1]
@@ -155,6 +220,60 @@ const sortProduct = (arr) => {
     //         })
     //     })
     // })
-}
 
-sortProduct(arr)
+
+
+// const checkbox = document.querySelector('#check')
+
+// const sort = (arr) => {
+//     checkbox.addEventListener('click',() => {
+//         content.innerHTML = ''
+//         if(checkbox.checked !== true) {
+//             content.innerHTML = ''
+//             arr.forEach(card => {
+//                 const div = document.createElement('div')
+//                       div.classList = 'card-block'  
+//                       div.innerHTML = `
+//                       <div class="card">
+//                       <div class="card-title">${card.title}</div>
+//                       <div class="card-image">
+//                           <img src="${card.img}" alt="">
+//                       </div>
+//                       <div class="card-footer">
+//                           <div class="card-weight">${card.weight}gr</div>
+//                           <div class="card-price">${card.price}</div>
+//                       </div>
+//                       <button class="card-btn">Delivery</button>
+//                   </div>
+//                       ` 
+//                       content.append(div)
+                      
+                      
+//             })
+//         }
+//         arr.sort((a,b) => a.weight - b.weight).forEach(el => {
+//                 const itemPr = document.createElement('div')
+//                        itemPr.innerHTML = `
+//                        <div class="card">
+//                        <div class="card-title">${el.title}</div>
+//                        <div class="card-image">
+//                            <img src="${el.img}" alt="">
+//                        </div>
+//                        <div class="card-footer">
+//                            <div class="card-weight">${el.weight}gr</div>
+//                            <div class="card-price">${el.price}</div>
+//                        </div>
+//                        <button class="card-btn">Delivery</button>
+//                    </div>
+//                        `
+//                        content.appendChild(itemPr)
+//             })
+//         })
+            
+     
+// }
+// sort(arr)
+
+
+
+
